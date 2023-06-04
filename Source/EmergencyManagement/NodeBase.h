f3c6969bc0a75b01a01878a25d7a9e0ac6bd7717
Bife13 <tomsy.ss@gmail.com>
2023-06-04 16:28:07 +0200

    Node Editor

---
+++ Source/EmergencyManagement/NodeBase.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "NodeBase.generated.h"
+
+/**
+ * 
+ */
+
+USTRUCT(BlueprintType)
+struct FNodeConnection
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input Node")
+	class UNodeBase* InputNode;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Output Node")
+	class UNodeBase* OutputNode;
+};
+
+UCLASS()
+class EMERGENCYMANAGEMENT_API UNodeBase : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadWrite)
+	UNodeBase* InputNode;
+	UPROPERTY(BlueprintReadWrite)
+	UNodeBase* OutputNode;
+	UPROPERTY(BlueprintReadWrite)
+	FNodeConnection Connection;
+	UPROPERTY(BlueprintReadWrite)
+	FString Name;
+	UPROPERTY(BlueprintReadWrite)
+	FString Description;
+	UPROPERTY(BlueprintReadWrite)
+	FColor Color;
+
+	// TODO ACTUAL NODE VARIABLES LATER
+
+	// Type
+	// Stress
+	// Location
+};
+
+
+
